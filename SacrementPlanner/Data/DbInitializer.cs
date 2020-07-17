12060b4613d6a91a36018e108aa111436392aab0
sarah <sarah@DESKTOP-OAUA1DU>
2020-07-17 15:10:21 -0700

    data annotation and seed data update

--- SacrementPlanner/Data/DbInitializer.cs
+++ SacrementPlanner/Data/DbInitializer.cs
@@ -112,6 +112,7 @@ namespace SacrementPlanner.Data
                     }
             };
 
+
             foreach (SpeakerAssignment ms in meetingSpeakers)
             {
                 context.SpeakerAssignment.Add(ms);
